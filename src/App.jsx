7e4789615c0f607b7ebec6fc66c79fdf86e33d72
Alok <rajlk45@gmail.com>
2025-07-07 15:58:40 +0530

    feat: update title and enhance About component with GSAP animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Hero from './components/hero.jsx'
 import About from './components/About.jsx'
+import Navbar from './components/Navbar.jsx'
 
 const App = () => {
   return (
-   <main className='relative min-h-screen w-screen overflow-x-hidden'>
+   <main className='relative min-h-screen w-screen overflow-x-hidden bg-zinc-700'>
+     <Navbar/>
      <Hero/>
-     
      <About/>
    </main>
   )
